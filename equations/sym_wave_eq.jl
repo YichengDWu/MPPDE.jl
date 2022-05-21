15a06e856c1179e4860889ef7a4c76bd09da0420
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2022-05-20 19:24:44 -0600

    sym attempt

---
+++ equations/sym_wave_eq.jl
@@ -0,0 +1,23 @@
+using ModelingToolkit
+
+@parameters x
+@variables t u(..)
+Dxx = Differential(x)^2
+Dtt = Differential(t)^2
+Dt = Differential(t)
+
+#2D PDE
+C=1
+eq  = Dtt(u(t,x)) ~ C^2*Dxx(u(t,x))
+
+# Initial and boundary conditions
+bcs = [u(t,0) ~ 0.,# for all t > 0
+       u(t,1) ~ 0.,# for all t > 0
+       u(0,x) ~ x*(1. - x), #for all 0 < x < 1
+       Dt(u(0,x)) ~ 0. ] #for all  0 < x < 1]
+
+# Space and time domains
+domains = [t ∈ (0.0,1.0),
+           x ∈ (0.0,1.0)]
+
+@named pde_system = PDESystem(eq,bcs,domains,[t,x],[u])
