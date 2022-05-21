eedb6900de5315db603e795f6906e94cdd1dd025
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2022-05-20 22:30:48 -0600

    defining combeq

--- equations/sym_comb_eq.jl
+++ equations/sym_comb_eq.jl
@@ -1,22 +1,31 @@
-using ModelingToolkit
+using ModelingToolkit, MethodOfLines, DomainSets
+using Symbolics: scalarize
 
-@parameters t x α β γ
-@variables u(..) δ(..)
+function CombEquation()
+       @parameters t x α β γ
+       @parameters A[1:5] ω[1:5] ℓ[1:5] ϕ[1:5]
 
-Dt = Differential(t)
-Dx = Differential(x)
-Dxx = Differential(x)^2
-Dxxx = Differential(x)^3
+       @variables u(..) 
 
-eq = Dt(u(t, x)) + 2.0 * α * u(t, x) * Dx(u(t, x)) - β * Dxx(u(t, x)) + γ * Dxxx(u(t, x)) ~ 0
+       δ(t,x) = sum(scalarize(A) .* sin.(scalarize(ω) .* t .+ π/8 .* scalarize(ℓ) .* x .+ scalarize(ϕ)))
 
-tmin,tmax = 0, 4
-xmin,xmax = 0, 16
+       Dt = Differential(t)
+       Dx = Differential(x)
+       Dxx = Differential(x)^2
+       Dxxx = Differential(x)^3
 
-domains = [x ∈ (xmin, xmax),
-              t ∈ (tmin, tmax)]
+       eq = Dt(u(t, x)) ~  -2.0 * α * u(t, x) * Dx(u(t, x)) + β * Dxx(u(t, x)) - γ * Dxxx(u(t, x)) + δ(t,x)
 
-bcs = [u(0,x) ~ δ(0,x),
-       u(t,xmin) ~ u(t,xmax)]
+       tmin,tmax = 0, 4
+       xmin,xmax = 0, 16
+
+       domains = [x ∈ Interval(xmin, xmax),
+                     t ∈ Interval(tmin, tmax)]
+
+       bcs = [u(0,x) ~ δ(0,x),
+              u(t,xmin) ~ u(t,xmax)]
+              
+       @named combeq = PDESystem(eq,bcs,domains,[t,x],[u(t,x)],[α, β, γ, A, ω, ℓ, ϕ])
        
-@named combeq = PDESystem(eq,bcs,domains,[t,x],[u(t,x)])
+       return combeq
+end
