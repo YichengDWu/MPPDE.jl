f0c7cd2301ffef9e63eef0af9f595f43166e492e
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2022-05-22 11:25:03 -0600

    ;

--- equations/sym_wave_eq.jl
+++ equations/sym_wave_eq.jl
@@ -20,4 +20,4 @@ bcs = [u(t,0) ~ 0.,# for all t > 0
 domains = [t ∈ (0.0,1.0),
            x ∈ (0.0,1.0)]
 
-@named pde_system = PDESystem(eq,bcs,domains,[t,x],[u])
+@named pde_system = PDESystem(eq,bcs,domains,[t,x],[u(t,x)])
