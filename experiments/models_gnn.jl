1cc3827dcb7cd1fe5968141a5af093c1b5eac08e
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2022-05-26 13:33:53 -0600

    add processor

--- experiments/models_gnn.jl
+++ experiments/models_gnn.jl
@@ -16,7 +16,7 @@ end
 
 Flux.@functor ProcessorLayer
 
-function ProcessorLayer(ch::Pair{Int,Int}, timewindow::Int, neqvar::Int = 0, dhidden::Int=128)
+function ProcessorLayer(ch::Pair{Int,Int}, timewindow::Int, neqvar::Int=0, dhidden::Int=128)
     din, dout = ch
     ϕ = Chain(
         Dense(2 * din + timewindow + 1 + neqvar, dhidden, swish),
@@ -29,41 +29,47 @@ function ProcessorLayer(ch::Pair{Int,Int}, timewindow::Int, neqvar::Int = 0, dhi
     ProcessorLayer(ϕ, ψ)
 end
 
-function (p::ProcessorLayer)(g::GNNGraph, ndata::NamedTuple{(:f, :u, :pos, :θ),NTuple{4, S}}) where {S<:AbstractMatrix}
+function (p::ProcessorLayer)(g::GNNGraph, ndata::NamedTuple{(:f, :u, :pos, :θ),NTuple{4,S}}) where {S<:AbstractMatrix}
     @unpack ϕ, ψ = p
     @unpack f, θ = ndata
-    
+
     function message(xi, xj, e)
         return ϕ(cat(xi.f, xj.f, xi.u - xj.u, xi.pos - xj.pos, xi.θ, dims=1))
     end
 
     m = propagate(message, g, +, xi=ndata, xj=ndata)
     update = ψ(cat(f, m, θ; dims=1))
-    x = size(update)[1] == size(f)[1] ? update + f : update
-    return x #TODO: add Instance Norm
+    newf = size(update)[1] == size(f)[1] ? update + f : update
+    return (f=newf, u=u, pos=pos, θ=θ) #TODO: add Instance Norm
 end
 
 
 function (p::ProcessorLayer)(g::GNNGraph)
-    GNNGraph(g, ndata=(f = p(g, g.ndata), u = g.ndata.u, pos = g.ndata.pos, θ = g.ndata.θ))
+    GNNGraph(g, ndata=(f=p(g, g.ndata), u=g.ndata.u, pos=g.ndata.pos, θ=g.ndata.θ))
 end
 
+function Processor(ch::Pair{Int,Int}, timewindow::Int, neqvar::Int=0, dhidden::Int=128, nlayers::Int=6)
+    @assert ch.first == ch.second
+    GNNChain([ProcessorLayer(ch, timewindow, neqvar, dhidden) for i in 1:nlayers]...)
+end
+
+
 function Decoder(timewindow::Int)
-    @assert timewindow ∈ (20,25,50)
+    @assert timewindow ∈ (20, 25, 50)
     if timewindow == 20
         return Chain(
-            Conv((15,),1=>8,swish;stride=4),
-            Conv((10,),8=>1,swish),
+            Conv((15,), 1 => 8, swish; stride=4),
+            Conv((10,), 8 => 1, swish),
         )
     elseif timewindow == 25
         return Chain(
-            Conv((16,),1=>8,swish;stride=3),
-            Conv((14,),8=>1,swish),
+            Conv((16,), 1 => 8, swish; stride=3),
+            Conv((14,), 8 => 1, swish),
         )
-    else 
+    else
         Chain(
-            Conv((12,),1=>8,swish;stride=2),
-            Conv((10,),8=>1,swish),
+            Conv((12,), 1 => 8, swish; stride=2),
+            Conv((10,), 8 => 1, swish),
         )
     end
 end
@@ -76,7 +82,7 @@ end
 
 Flux.@functor MP_PDE_solver
 
-function MP_PDESolver(pde::PDESystem; timewindow::Int = 25, dhidden::Int=128, nlayer::Int = 6, eqvar::NamedTuple=(;))
+function MP_PDESolver(pde::PDESystem; timewindow::Int=25, dhidden::Int=128, nlayer::Int=6, eqvar::NamedTuple=(;))
     """
     input: Temporal × (Spatial × N)
     """
@@ -84,21 +90,22 @@ function MP_PDESolver(pde::PDESystem; timewindow::Int = 25, dhidden::Int=128, nl
     MP_PDE_solver(
         pde,
         Encoder(timewindow, neqvar, dhidden),
-        Processor(dhidden=>dhidden, timewindow, neqvar, dhidden),
+        Processor(dhidden => dhidden, timewindow, neqvar, dhidden),
         Decoder(timewindow)
     )
 end
 
-function (p::MP_PDE_solver)(g::GNNGraph, data::NamedTuple)
+function (p::MP_PDE_solver)(data::NamedTuple)
     """
+    Push u[k-K:k] tp u[k:k+K]
     input:
-        GNNGraph with ndata set up
-        data: (u,x,t,theta)
-        
-    For simplicity α, β, γ are all fed into NN
+        data: (u,x,t,θ)   #u is already sample to be (K, Nx * batch_size)
+    
     """
-    f = encoder(cat(u,x,t,θ))
-    g.ndata.f = f
-    h = processor(g)
+    @unpack u, x, t, θ = data   #TODO: add norm
+    f = encoder(vcat(u, x, t, θ))
+    ndata = (f=f, u=u, x=x, t=t, θ=θ)
+    g = constructgraph()
+    h = processor(g, ndata).f
     u = decoder(h)
 end
